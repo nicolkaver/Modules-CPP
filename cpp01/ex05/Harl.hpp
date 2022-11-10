8738543fd0030a216a944ab5e251ef18c822a0a5
Nikola BORATKOVA <nboratko@e1r7p8.clusters.42paris.fr>
2022-11-10 15:03:56 +0100

    added Harl

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -4,6 +4,21 @@
 # include <iostream>
 # include <string>
 
+# define DEBUG 0
+# define INFO 1
+# define WARNING 2
+# define ERROR 3
+# define TOTAL_NUM_OF_LEVEL 4
+
+class Harl;
+
+typedef struct  s_complain
+{
+	std::string level;
+	void        (Karen::*func_ptr)(void);
+}               t_complain;
+
+
 class Harl {
     public:
         Harl(void);
@@ -12,10 +27,11 @@ class Harl {
         void    complain(std::string level);
 
     private:
-        void    debug(void);
-        void    info(void);
-        void    warning(void);
-        void    error(void);
+        void        _debug(void);
+        void        _info(void);
+        void        _warning(void);
+        void        _error(void);
+        t_complain	_complain_list[TOTAL_NUM_OF_LEVEL];
 }
 
 #endif
